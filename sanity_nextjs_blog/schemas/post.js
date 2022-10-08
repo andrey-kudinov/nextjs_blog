206f786b165c86d88012bc2f680e0770f3cf69d7
Andrey Kudinov <andrey.kudinov@avaloninnovations.com.au>
2022-10-08 09:42:39 +0000

    Add Sanity

---
+++ sanity_nextjs_blog/schemas/post.js
@@ -0,0 +1,12 @@
+export default {
+  name: 'animal',
+  type: 'document',
+	title: 'Post 1',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Name'
+    }
+  ]
+}
---
+++ sanity_nextjs_blog/schemas/schema.js
@@ -0,0 +1,10 @@
+import createSchema from 'part:@sanity/base/schema-creator'
+import schemaTypes from 'all:part:@sanity/base/schema-type'
+import post from './post';
+
+export default createSchema({
+  name: 'default',
+  types: schemaTypes.concat([
+    post,
+  ]),
+})
