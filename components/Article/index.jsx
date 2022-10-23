539d369771fe3a281c16dd25f3b08fb6a3c51ecb
Andrey Kudinov <andrey.kudinov@avaloninnovations.com.au>
2022-10-23 18:17:18 +0000

    Create Article

--- components/Article/index.jsx
+++ components/Article/index.jsx
@@ -1,6 +1,15 @@
 import styles from './index.module.scss'
 import cl from 'classnames'
+import Link from 'next/link'
+import { AiOutlineArrowLeft } from 'react-icons/ai'
 
-export const Article = ({ className, children }) => (
-  <article className={cl(className, styles.article)}>{children}</article>
+export const Article = ({ className, children, backUrl }) => (
+  <article className={cl(className, styles.article)}>
+    <Link href={backUrl}>
+      <a className={styles.back}>
+        <AiOutlineArrowLeft />
+      </a>
+    </Link>
+    <div className={styles.content}>{children}</div>
+  </article>
 )
