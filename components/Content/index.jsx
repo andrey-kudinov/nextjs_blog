763c626de4dd16b6233fc46a4745a605e78b1525
Andrey Kudinov <andrey.kudinov@avaloninnovations.com.au>
2022-10-23 18:17:28 +0000

    Create Content

--- components/Content/index.jsx
+++ components/Content/index.jsx
@@ -1,4 +1,15 @@
 import styles from './index.module.scss'
 import cl from 'classnames'
+import SanityBlockContent from '@sanity/block-content-to-react'
+import { clientConfig } from '../../lib/client'
 
-export const Content = ({ className }) => <div>Content</div>
+export const Content = ({ className, body }) => (
+  <div className={cl(styles.content, className)}>
+    <SanityBlockContent
+      blocks={body}
+      imageOptions={{ w: 1120, h: 750, fit: 'max' }}
+      projectId={clientConfig.projectId}
+      dataset={clientConfig.dataset}
+    />
+  </div>
+)
