74e8f4cb421e80dd3c49fbf445f6222b80e4cfcf
Andrey Kudinov <andrey.kudinov@avaloninnovations.com.au>
2023-03-15 18:15:38 +0400

    Clean up a bit

--- components/Post/index.jsx
+++ components/Post/index.jsx
@@ -1,30 +1,20 @@
-import styles from './index.module.scss'
-import cl from 'classnames'
-import Link from 'next/link'
-import Image from 'next/image'
-import { urlFor } from '../../lib/client'
-import { Title } from '../Title'
+import styles from './index.module.scss';
+import cl from 'classnames';
+import Link from 'next/link';
+import Image from 'next/image';
+import { urlFor } from '../../lib/client';
+import { Title } from '../Title';
 
 export const Post = ({ className, image, title, description, slug }) => (
-  (<Link
-    href={`/post/${encodeURIComponent(slug.current)}`}
-    class={cl(className, styles.post)}
-    className={styles.link}>
-
+  <Link href={`/post/${encodeURIComponent(slug.current)}`} className={cl(styles.link, styles.post)}>
     <Title type='small' className={styles.title}>
       {title}
     </Title>
     <div className={styles.content}>
       <div>
-        <Image
-          src={urlFor(image).url()}
-          alt={image.caption}
-          width="100px"
-          height="100px"
-        />
+        <Image src={urlFor(image).url()} alt={image.caption} width='100' height='100' />
       </div>
       <div className={styles.description}>{description}</div>
     </div>
-
-  </Link>)
-)
+  </Link>
+);
